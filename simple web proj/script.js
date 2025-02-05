e7c11d6593318944be8280ffa62f152343b3f99a
maki-debuggers <itsblitzflores@gmail.com>
2025-02-05 17:11:17 +0800

    Add files via upload

---
+++ simple web proj/script.js
@@ -0,0 +1,10 @@
+function yesnextPage() {
+    window.location.href = "yes.html";
+}
+
+function moveButton() {
+    var x = Math.random() * (window.innerWidth - document.getElementById('noButton').offsetWidth) - 85;
+    var y = Math.random() * (window.innerHeight - document.getElementById('noButton').offsetHeight) - 48;
+    document.getElementById('noButton').style.left = `${x}px`;
+    document.getElementById('noButton').style.top = `${y}px`;
+}
